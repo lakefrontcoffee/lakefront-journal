fdcff2129df3ee142ae49c9256c9c0da313633b1
lakefrontcoffee <DRKONGRKL@GMAIL.COM>
2025-07-20 13:51:16 -0500

    folders upload

---
+++ components/Day1.tsx
@@ -0,0 +1,8 @@
+export default function Day1() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>LakeFront Journal: Day 1</h1>
+      <p>Welcome to your first day of reflections ☕</p>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import Day1 from '../components/Day1';
+
+export default function Home() {
+  return <Day1 />;
+}
