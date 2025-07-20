2b5c2536174debf23539707b34123e8b3a2cffdb
lakefrontcoffee <DRKONGRKL@GMAIL.COM>
2025-07-20 15:37:02 -0500

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-900">
-      <h1 className="text-3xl font-bold">LakeFront Journal: Day 1</h1>
-    </main>
-  )
-}
